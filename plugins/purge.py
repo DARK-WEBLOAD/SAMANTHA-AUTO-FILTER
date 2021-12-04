4270431b3538ed1d675f517e5707c2e3726f40da
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-04 08:58:43 +0530

    Update purge.py

--- plugins/purge.py
+++ plugins/purge.py
@@ -2,7 +2,7 @@
 Syntax: .purge"""
 
 import asyncio
-from pyrogram import Client, filters🤭
+from pyrogram import Client, filters
 
 from plugins.helper_functions.admin_check import admin_check
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
