00f747660d1f415d6d33ae7899cde36c3efdcb03
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-04 08:27:28 +0530

    Update restrict.py

--- plugins/restrictions/restrict.py
+++ plugins/restrictions/restrict.py
@@ -31,7 +31,7 @@ async def mute_user(_, message):
     else:
         if str(user_id).lower().startswith("@"):
             await message.reply_text(
-                "👍🏻 "
+                "⭕️ "
                 f"{user_first_name}"
                 " Lavender's mouth is shut! 🤐"
             )
