647893b962483bf90bf60e20f7598db568daa013
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-04 08:45:59 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -3,7 +3,7 @@ from pyrogram import Client, emoji, filters
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
 from database.ia_filterdb import get_search_results
-from utils import is_subscribed, get_size
+from utils import is_subscribed, get_size🤭
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
