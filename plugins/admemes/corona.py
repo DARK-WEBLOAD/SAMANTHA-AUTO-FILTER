c4cbbfaf088e7487607c7f82bc3be3ff0e8dfed8
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-06 10:24:56 +0530

    Update corona.py

--- plugins/admemes/corona.py
+++ plugins/admemes/corona.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API = "https://api.sumanjay.cf/covid/?country="
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join our Channel ⚙', url='https://t.me/team_annaben')]])
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ⚙', url='https://t.me/joinchat/FYBCIcMfr0AyOTFl')]])
 
 @Client.on_message(filters.command("covid"))
 async def reply_info(client, message):
