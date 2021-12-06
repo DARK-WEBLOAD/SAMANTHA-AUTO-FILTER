1fb92fb55b4179ddc628b50d4ea5f087aad237b3
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-06 10:08:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -532,6 +532,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
+    elif query.data == "corona":
+        buttons = [[
+            InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.CORONA_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
     elif query.data == "song":
         buttons = [[
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help')
